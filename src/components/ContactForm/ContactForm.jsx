7b05c9660a3d93946def1db6cbda77b96d0e5600
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-05-01 22:27:44 +0200

    hw

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,35 @@
+import css from "./ContactForm.module.css";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import { nanoid } from "nanoid";
+import { useId } from "react";
+import * as Yup from "yup"
+
+const FeedbackSchema = Yup.object().shape({
+    name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required"),
+  number: Yup.string().matches(/^\d{3}-\d{2}-\d{2}$/, "Invalid phone number format").required("Required"),
+})
+
+export default function ContactForm({ onAdd }) {
+    const handleSubmit = (values, action) => {
+        const newContact = {
+         id: nanoid(),
+        name: values.name,
+        number: values.namber
+     }   
+        onAdd(newContact);
+        action.resetForm();
+    }
+    const nameFieldId = useId();
+    const numberFieldId = useId();
+    
+    return (
+        <Formik initialValues={{ name:"", number:""}} onSubmit={ handleSubmit} validationSchema={FeedbackSchema}>
+            <Form className={css.formContainer}>
+                <label htmlFor={nameFieldId}>Name</label>
+                <Field type='text' name='name' id={nameFieldId } />
+                <label htmlFor={numberFieldId}>Number</label>
+                <Field type='text' name='number' id={ numberFieldId} />
+            <button className={css.btn} type='submit'>Add contact</button>
+            </Form>
+        </Formik> 
+    )}
